762120875eb0b6e8d38d29193c1c829cb7304b26
oddcelot <mail@oddobsessions.com>
2019-03-26 00:29:25 +0100

    Fix code style

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import svg4everybody from 'svg4everybody';
+import svg4everybody from 'svg4everybody'
 
-document.addEventListener("DOMContentLoaded",function(){
-    svg4everybody();
-});
+document.addEventListener('DOMContentLoaded', function() {
+  svg4everybody()
+})
